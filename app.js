42695549a4b363a9d6f45762331a89bf9d3f5d0e
jina <jonyvaishnav81@gmail.com>
2024-07-22 02:49:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require("path");
+
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+
+io.on("connection", function(socket){socket.on("send-location", function(data){
+    io.emit("receive-location", {id: socket.id, ...data});
+});
+
+
+socket.on("disconnect", function(){
+    io.emit("user-disconnect", socket.id);
+})
+    console.log("connected");
+});
+
+
+app.get("/", function (req, res){
+    res.render("index");
+});
+
+server.listen(3000);
